d849c0a979f77870621e84cb2b1930e016032ccc
gurdilou <thomas.luce7342@gmail.com>
2016-11-18 23:43:53 +0100

    feat : design selected job detail

--- client/app/views/map/map-job-detail.component.ts
+++ client/app/views/map/map-job-detail.component.ts
@@ -47,4 +47,57 @@ export class MapJobDetailComponent  implements OnInit {
     this.onJobPopupClose.emit(undefined);
   }
 
+  /**
+   * Lorsque l'utilisateur veut voir tout le descriptif d'une offre
+   */
+  protected onExpandDesc(event : any) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('.ui.long.modal.offer-desc-long')
+      .modal({
+        blurring: true
+      })
+      .modal('setting', 'transition', 'vertical flip')
+      .modal('show');
+  }
+
+  onPreviousOffer() {
+    if(this.selectedIndex > 0) {
+      let self = this;
+      let offerContent = $('.offer-body');
+      offerContent.transition({
+        animation: 'fade right',
+        duration: 100,
+      }).transition({
+        animation: 'fade left',
+        duration: 25,
+        onComplete : function() {
+          self.selectedIndex--;
+          self.selectedOffer = self.offers[self.selectedIndex];
+          offerContent.removeClass("visible");
+        }
+      });
+    }
+
+  }
+  onNextOffer() {
+    if(this.selectedIndex < this.offers.length) {
+      let self = this;
+      let offerContent = $('.offer-body');
+      offerContent.transition({
+        animation: 'fade left',
+        duration: 100,
+      }).transition({
+        animation: 'fade right',
+        duration: 25,
+        onComplete : function() {
+          self.selectedIndex++;
+          self.selectedOffer = self.offers[self.selectedIndex];
+          offerContent.removeClass("visible");
+        }
+      });
+
+    }
+  }
+
 }
